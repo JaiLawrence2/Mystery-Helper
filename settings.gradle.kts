0d35b4847a5e70a85d0051afac28a53911469d54
JaiLawrence <jailon706@icloud.com>
2024-01-23 11:47:06 -0600

    Initial Commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Mystery Helper CS408_1A"
+include(":app")
